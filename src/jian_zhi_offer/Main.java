0cfb1b0f057c042f6a80fafb6ab925f9eedbf729
boomzy <542061862@qq.com>
2022-03-20 15:34:24 +0800

    add

--- src/jian_zhi_offer/Main.java
+++ src/jian_zhi_offer/Main.java
@@ -2,10 +2,7 @@ package jian_zhi_offer;
 
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 剑指offer
@@ -1127,6 +1124,83 @@ public class Main {
         }
 
         return isTree1HaveTree2(A.left, B.left) && isTree1HaveTree2(A.right, B.right);
+    }
+
+    /**
+     * 剑指 Offer 27. 二叉树的镜像
+     * <p>
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     * 例如输入：
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * <p>
+     * 镜像输出：
+     * <p>
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode mirrorTree(TreeNode root) {
+        /*
+            先前序遍历这个树的每个节点，如果遍历到的节点有子节点，就交换
+            当交换完所有非叶子节点的左，右子节点之后，就得到了镜像
+         */
+        doMirrorTreeRecursion(root);
+//        doMirrorTreeIteratively(root);
+        return root;
+    }
+
+    // 递归方式
+    private void doMirrorTreeRecursion(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            return;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        if (root.left != null) {
+            doMirrorTreeRecursion(root.left);
+        }
+        if (root.right != null) {
+            doMirrorTreeRecursion(root.right);
+        }
+    }
+
+    // 迭代方式
+    private void doMirrorTreeIteratively(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
     }
 }
 
